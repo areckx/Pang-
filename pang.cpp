ae10ad8f136ebab58f6f5fb307c3f2507e4906ca
Alex Loranger <alex.loranger.1988@gmail.com>
2013-12-19 17:55:26 -0800

    Create pang.cpp

---
+++ pang.cpp
@@ -0,0 +1,18 @@
+// Pang.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Game.h"
+
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+//int main(int argc, char** argv)
+{
+	
+	Game::Start();
+
+	return 0;
+}
+
